a40ab20064ba3dee5a94b3f4af481c5fec0f0916
Darwin Joel Arroyo Pérez <darwinjoelap@gmail.com>
2026-08-20 02:18:52 -0400

    feat: apply color_primario dynamically from DB config

--- pediae/context_processors.py
+++ pediae/context_processors.py
@@ -8,14 +8,17 @@ def consultorio_info(request):
             'CONSULTORIO_NOMBRE': 'Pediae',
             'CONSULTORIO_ESPECIALIDAD': '',
             'TENANT_PREFIX': '',
+            'COLOR_PRIMARIO': '#2AACA8',
         }
     try:
         config = tenant.config
+        color = config.color_primario or '#2AACA8'
         return {
             'CONSULTORIO_NOMBRE': config.nombre_display(),
             'CONSULTORIO_ESPECIALIDAD': config.especialidad,
             'CONSULTORIO_CONFIG': config,
             'TENANT_PREFIX': f'/t/{tenant.slug}',
+            'COLOR_PRIMARIO': color,
         }
     except ConfigConsultorio.DoesNotExist:
         return {
@@ -23,4 +26,5 @@ def consultorio_info(request):
             'CONSULTORIO_ESPECIALIDAD': '',
             'CONSULTORIO_CONFIG': None,
             'TENANT_PREFIX': f'/t/{tenant.slug}',
+            'COLOR_PRIMARIO': '#2AACA8',
         }
