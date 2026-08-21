b443e988e08082028680e73f4c0cda493e13537b
Darwin Joel Arroyo Pérez <darwinjoelap@gmail.com>
2026-08-21 00:29:59 -0400

    feat: agrega especialidad, credenciales y MPPS en formulario de usuario

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,8 @@ class UsuarioCrearForm(forms.ModelForm):
 
     class Meta:
         model = Usuario
-        fields = ['first_name', 'last_name', 'username', 'email', 'rol', 'sexo']
+        fields = ['first_name', 'last_name', 'username', 'email', 'rol', 'sexo',
+                  'especialidad', 'credenciales', 'numero_mpps']
 
     def clean_password2(self):
         p1 = self.cleaned_data.get('password1')
@@ -30,7 +31,8 @@ class UsuarioCrearForm(forms.ModelForm):
 class UsuarioEditarForm(forms.ModelForm):
     class Meta:
         model = Usuario
-        fields = ['first_name', 'last_name', 'email', 'rol', 'sexo', 'is_active']
+        fields = ['first_name', 'last_name', 'email', 'rol', 'sexo', 'is_active',
+                  'especialidad', 'credenciales', 'numero_mpps']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
