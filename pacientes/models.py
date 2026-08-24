3bd83781441bc7b217cb0617f38b8ffdbcc4fed2
Darwin Joel Arroyo Pérez <darwinjoelap@gmail.com>
2026-08-23 20:53:31 -0400

    feat(pacientes): teléfono opcional

--- pacientes/models.py
+++ pacientes/models.py
@@ -71,7 +71,7 @@ class Paciente(models.Model):
         default='',
     )
     fecha_nacimiento = models.DateField(null=True, blank=True, verbose_name='Fecha de nacimiento')
-    telefono = models.CharField(max_length=20, verbose_name='Teléfono')
+    telefono = models.CharField(max_length=20, blank=True, default='', verbose_name='Teléfono')
     email = models.EmailField(blank=True, verbose_name='Email')
     direccion = models.TextField(blank=True, verbose_name='Dirección')
     seguro_medico = models.CharField(max_length=100, blank=True, verbose_name='Seguro médico')
