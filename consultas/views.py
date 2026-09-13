63a742e9dcda5e7fe8119654c01c7747d019de5a
Darwin Joel Arroyo Pérez <darwinjoelap@gmail.com>
2026-09-13 18:06:29 -0400

    fix: add missing JsonResponse import in views.py

--- consultas/views.py
+++ consultas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, JsonResponse
 from django.contrib import messages
 from pediae.decorators import tenant_login_required as login_required
 from pacientes.models import Paciente
