e5d6938a6d7f9e5aa16478f9f914b687c728bcf0
Darwin Joel Arroyo Pérez <darwinjoelap@gmail.com>
2026-09-13 11:08:16 -0400

    fix: merge migration 0016 para resolver conflicto 0015

---
+++ consultas/migrations/0016_merge_medicamento_migrations.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('consultas', '0015_medicamento_dosificacion'),
+        ('consultas', '0015_medicamento_dosis_fija'),
+    ]
+
+    operations = [
+    ]
